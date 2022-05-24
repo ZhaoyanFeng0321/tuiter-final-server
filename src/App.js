03b748d219c554a3c08b672c5099b8a1219efe4b
Zhaoyan <irisfeng0321@outlook.com>
2022-05-24 00:04:01 -0400

    fix routes bugs

--- src/App.js
+++ src/App.js
@@ -2,11 +2,56 @@ import React from "react";
 import './styles.css';
 
 import Tuiter from "./components/tuiter";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Navigation from "./components/navigation";
+import Home from "./components/home";
+import {Login} from "./components/profile/login";
+import Signup from "./components/profile/signup";
+import MyHome from "./components/home/my-home";
+import Explore from "./components/explore";
+import Notifications from "./components/notifications";
+import Messages from "./components/messages";
+import Bookmarks from "./components/bookmarks";
+import Lists from "./components/lists";
+import Profile from "./components/profile";
+import EditProfile from "./components/profile/edit-profile";
+import Following from "./components/profile/following";
+import Followers from "./components/profile/followers";
+import More from "./components/more";
+import WhatsHappening from "./components/whats-happening";
+import WhoToFollowList from "./components/whotofollowlist";
 
 function App() {
   return (
-    <Tuiter/>
-);
+      <BrowserRouter>
+              <Routes>
+                <Route element={{Tuiter}}/>
+                <Route index element={<Home/>}/>
+                <Route path="login" element={<Login/>}/>
+                <Route path="signup" element={<Signup/>}/>
+                <Route path="tuiter" element={<Home/>}>
+                  <Route path=":uid" element={<Home/>}/>
+                </Route>
+                <Route path="home" element={<MyHome/>}>
+                  <Route path=":uid" element={<MyHome/>}/></Route>
+                <Route path="explore" element={<Explore/>}/>
+                <Route path="notifications" element={<Notifications/>}/>
+                <Route path="messages" element={<Messages/>}/>
+                <Route path="bookmarks" element={<Bookmarks/>}/>
+                <Route path="lists" element={<Lists/>}/>
+                <Route path="profile" element={<Profile/>}>
+                  <Route path=":username" element={<Profile/>}>
+                    <Route path="edit" element={<EditProfile/>}/>
+                    <Route path="following" element={<Following/>}/>
+                    <Route path="followers" element={<Followers/>}/>
+                    <Route path="*" element={<Profile/>}/>
+                  </Route>
+                </Route>
+                <Route path="more" element={<More/>}/>
+              </Routes>
+
+      </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/tuiter/index.js
+++ src/components/tuiter/index.js
@@ -1,59 +1,23 @@
 import React, {useEffect, useState} from "react";
 import Navigation from "../navigation";
 import WhatsHappening from "../whats-happening";
-import {BrowserRouter, Routes, Route, HashRouter, useNavigate} from "react-router-dom";
-import Home from "../home";
-import Bookmarks from "../bookmarks";
-import Profile from "../profile";
-import './tuiter.css'
-import EditProfile from "../profile/edit-profile";
-import Explore from "../explore";
-import Notifications from "../notifications";
-import Messages from "../messages";
-import Lists from "../lists";
-import More from "../more";
-import {Login} from "../profile/login";
-import Signup from "../profile/signup";
-import * as service from "../../services/auth-service";
-import Following from "../profile/following";
-import Followers from "../profile/followers";
-import WhoToFollowList from "../whotofollowlist";
-import MyHome from "../home/my-home";
 
-function Tuiter() {
+import './tuiter.css'
+
+import WhoToFollowList from "../whotofollowlist";
+import {Outlet} from "react-router-dom";
+
+const Tuiter=() =>{
 
     return (
-        <BrowserRouter>
+        <>
             <div className="container">
                 <div className="ttr-tuiter">
                     <div className="ttr-left-column">
                         <Navigation/>
                     </div>
                     <div className="ttr-center-column">
-                        <Routes>
-                            <Route index element={<Home/>}/>
-                            <Route path="login" element={<Login/>}/>
-                            <Route path="signup" element={<Signup/>}/>
-                            <Route path="tuiter" element={<Home/>}>
-                                <Route path=":uid" element={<Home/>}/>
-                            </Route>
-                            <Route path="home" element={<MyHome/>}>
-                                <Route path=":uid" element={<MyHome/>}/></Route>
-                            <Route path="explore" element={<Explore/>}/>
-                            <Route path="notifications" element={<Notifications/>}/>
-                            <Route path="messages" element={<Messages/>}/>
-                            <Route path="bookmarks" element={<Bookmarks/>}/>
-                            <Route path="lists" element={<Lists/>}/>
-                            <Route path="profile" element={<Profile/>}>
-                                <Route path=":username" element={<Profile/>}>
-                                    <Route path="edit" element={<EditProfile/>}/>
-                                    <Route path="following" element={<Following/>}/>
-                                    <Route path="followers" element={<Followers/>}/>
-                                    <Route path="*" element={<Profile/>}/>
-                                </Route>
-                            </Route>
-                            <Route path="more" element={<More/>}/>
-                        </Routes>
+                        <Outlet/>
                     </div>
                     <div className="ttr-right-column">
                         <WhatsHappening/>
@@ -63,8 +27,8 @@ function Tuiter() {
                     </div>
                 </div>
             </div>
-        </BrowserRouter>
-    );
+        </>
+    )
 }
 
 export default Tuiter;
